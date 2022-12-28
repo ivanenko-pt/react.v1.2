83e4f39b7a91aae39dfa52747f385f7d8de13c70
Serhii <rfrf198231@gmail.com>
2022-12-28 23:45:12 +0000

    use-card-comp

--- side-menu/src/components/Users/AddUser.js
+++ side-menu/src/components/Users/AddUser.js
@@ -1,20 +1,20 @@
 import React from "react";
 
 const AddUser = (props) => {
-    const addUserHandler = (event) => {
-        event.preventDefaul();
+    const AddUser = (e) => {
+        e.preventDefault();
     };
 
     return (
-        <form onSubmit={addUserHandler}>
+        <form onSubmit={AddUser}>
             <label htmlFor="username">Username</label>
             <input id="username" type="text" />
 
-            <label htmlFor="username">Age(Years)</label>
+            <label>Year(age)</label>
             <input id="age" type="number" />
-
             <button type="submit">Add User</button>
         </form>
     );
 };
+
 export default AddUser;
