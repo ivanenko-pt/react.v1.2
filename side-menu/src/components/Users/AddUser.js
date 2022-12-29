8a62d04ddf0269a157925a8a1a761b4b3d388b26
Serhii <rfrf198231@gmail.com>
2022-12-29 10:38:03 +0000

    resset-logic

--- side-menu/src/components/Users/AddUser.js
+++ side-menu/src/components/Users/AddUser.js
@@ -10,12 +10,24 @@ const AddUser = (props) => {
 
     const addUserHandler = (event) => {
         event.preventDefault();
+        if (
+            enteredUsername.trim().length === 0 ||
+            enteredAge.trim().length === 0
+        ) {
+            return;
+        }
+        if (+enteredAge < 1) {
+            return;
+        }
         console.log(enteredAge, enteredUsername);
+        setEnteredUsername("");
+        setEnteredAge("");
     };
 
     const usernameChangeHandler = (event) => {
         setEnteredUsername(event.target.value);
     };
+
     const ageChangeHandler = (event) => {
         setEnteredAge(event.target.value);
     };
@@ -27,11 +39,17 @@ const AddUser = (props) => {
                 <input
                     id="username"
                     type="text"
+                    value={enteredUsername}
                     onChange={usernameChangeHandler}
                 />
 
                 <label>Years (age)</label>
-                <input id="age" type="number" onChange={ageChangeHandler} />
+                <input
+                    id="age"
+                    type="number"
+                    value={enteredAge}
+                    onChange={ageChangeHandler}
+                />
                 <Button type="submit">Add User</Button>
             </form>
         </Card>
