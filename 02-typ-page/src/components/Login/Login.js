cb10f424bca4789888ab9e3dc08170e6457fa4f3
Serhii <rfrf198231@gmail.com>
2023-01-06 15:46:58 +0000

    add-refs

--- 02-typ-page/src/components/Login/Login.js
+++ 02-typ-page/src/components/Login/Login.js
@@ -1,4 +1,10 @@
-import React, { useState, useEffect, useReducer, useContext } from "react";
+import React, {
+    useState,
+    useEffect,
+    useReducer,
+    useContext,
+    useRef,
+} from "react";
 
 import Card from "../UI/Card/Card";
 import Button from "../UI/Button/Button";
@@ -29,8 +35,9 @@ const passwordReducer = (state, action) => {
     return { value: "", isValid: false };
 };
 
-const Login = () => {
+const Login = (props) => {
     const ctx = useContext(AuthContext);
+
     /*Use state
     const [enteredEmail, setEnteredEmail] = useState("");
     const [emailIsValid, setEmailIsValid] = useState();
@@ -100,15 +107,26 @@ const Login = () => {
         dispatchPassword({ type: "INPUT_BLUR" });
     };
 
+    // useRef
+    const emailInputRef = useRef();
+    const passwordInputRef = useRef();
+
     const submitHandler = (event) => {
         event.preventDefault();
-        ctx.onLogin(emailState.value, passwordState.value);
+        if (formIsValid) {
+            ctx.onLogin(emailState.value, passwordState.value);
+        } else if (!emailIsValid) {
+            emailInputRef.current.focus();
+        } else {
+            passwordInputRef.current.focus();
+        }
     };
 
     return (
         <Card className={classes.login}>
             <form onSubmit={submitHandler}>
                 <Input
+                    ref={emailInputRef}
                     id="email"
                     label="E-Mail"
                     type="email"
@@ -118,6 +136,7 @@ const Login = () => {
                     onBlur={validateEmailHandler}
                 />
                 <Input
+                    ref={passwordInputRef}
                     id="password"
                     label="Password"
                     type="password"
@@ -127,11 +146,7 @@ const Login = () => {
                     onBlur={validatePasswordHandler}
                 />
                 <div className={classes.actions}>
-                    <Button
-                        type="submit"
-                        className={classes.btn}
-                        disabled={!formIsValid}
-                    >
+                    <Button type="submit" className={classes.btn}>
                         Login
                     </Button>
                 </div>
