effb041b7c5830a47731b0bc9e7cb404c4a4c5c1
Serhii <rfrf198231@gmail.com>
2023-01-07 22:10:27 +0000

    work

--- order-food-app/src/components/UI/Input.js
+++ order-food-app/src/components/UI/Input.js
@@ -2,10 +2,10 @@ import classes from "./Input.module.css";
 
 const Input = (props) => {
     return (
-        // <div className={classes.input}>
-        //     <label htmlFor={props.input.id}>{props.label}</label>
-        //     <input {...props.input} />
-        // </div>
+        <div className={classes.input}>
+            <label htmlFor={props.input.id}>{props.label}</label>
+            <input {...props.input} />
+        </div>
     );
 };
 
