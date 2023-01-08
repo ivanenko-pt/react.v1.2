b65f441f25c66ccf4d5c98848a1c74e084858814
Serhii <rfrf198231@gmail.com>
2023-01-08 18:15:07 +0000

    add modal via portal

--- order-food-app/src/components/Cart/Cart.js
+++ order-food-app/src/components/Cart/Cart.js
@@ -1,9 +1,10 @@
+import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
     const cartItems = (
         <ul className={classes["cart-items"]}>
-            {[{ id: "c1", name: "sushi", amount: 2, price: 12.22 }].map(
+            {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map(
                 (item) => (
                     <li>{item.name}</li>
                 )
@@ -12,17 +13,18 @@ const Cart = (props) => {
     );
 
     return (
-        <div>
+        <Modal>
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
-                <span>43.33</span>
+                <span>35.62</span>
             </div>
             <div className={classes.actions}>
                 <button className={classes["button--alt"]}>Close</button>
                 <button className={classes.button}>Order</button>
             </div>
-        </div>
+        </Modal>
     );
 };
+
 export default Cart;
